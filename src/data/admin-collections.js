b7b51629a6fb16f72778b68131914f44ebaffef3
pellegrinottijoshua-hash <pellegrinottijoshua@gmail.com>
2026-05-01 00:02:31 +0200

    admin: add collection cool-pok-mon

--- src/data/admin-collections.js
+++ src/data/admin-collections.js
@@ -1,2 +1,10 @@
 // This file is managed by the JAYL admin panel. Do not edit manually.
-export const adminCollections = []
+export const adminCollections = [
+  {
+    "id": "cool-pok-mon",
+    "name": "cool pokémon",
+    "color": "#f2be64",
+    "coverImage": "",
+    "order": 0
+  }
+]
