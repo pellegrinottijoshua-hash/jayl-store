715f5e82004edb10fdb420b18ca9375bafbf3c1a
pellegrinottijoshua-hash <pellegrinottijoshua@gmail.com>
2026-05-01 00:34:27 +0200

    admin: add mew-pok-mon-t-shirt-retro-90s-anime-fan-art-gift

--- src/data/admin-products.js
+++ src/data/admin-products.js
@@ -1,2 +1,241 @@
 // This file is managed by the JAYL admin panel. Do not edit manually.
-export const adminProducts = []
+export const adminProducts = [
+  {
+    "id": "mew-pok-mon-t-shirt-retro-90s-anime-fan-art-gift",
+    "section": "objects",
+    "collection": "pokemon-logos",
+    "name": "Mew Pokémon T-Shirt | Retro 90s Anime Fan Art Gift",
+    "subtitle": "pokemon-logos",
+    "price": 1999,
+    "currency": "eur",
+    "description": "Celebrate your love for Pokémon with our Mew T-Shirt, a nostalgic nod to the 90s anime era. Crafted from soft, breathable cotton, this shirt features vibrant Gelato print quality that captures the playful essence of Mew in stunning detail. The retro art style is perfect for fans who appreciate a blend of humor and nostalgia. Whether you're out with friends or relaxing at home, this T-shirt is a stylish and comfortable choice. Ideal as a gift for Pokémon lovers, this shirt will surely spark joy and smiles. Wear your fandom proudly and embrace the whimsy of your favorite pocket monster!",
+    "altText": "Mew Pokémon T-Shirt featuring vibrant retro art style and Gelato print quality.",
+    "details": [
+      "Printed and fulfilled via Gelato"
+    ],
+    "sizes": [
+      {
+        "id": "one-size",
+        "label": "One Size",
+        "price": 1999
+      }
+    ],
+    "image": "/images/mew-pok-mon-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift/mew-pok-mon-t-shirt-cool-anime-fan-art-gift-r-mockup-01.jpg",
+    "images": [
+      "/images/mew-pok-mon-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift/mew-pok-mon-t-shirt-cool-anime-fan-art-gift-r-mockup-01.jpg"
+    ],
+    "tags": [
+      "mew t-shirt",
+      "pokemon shirt",
+      "anime fan art",
+      "90s style",
+      "funny gift",
+      "anime merchandise",
+      "retro clothing",
+      "geek fashion",
+      "pop culture",
+      "gift for him",
+      "gift for her",
+      "collector item",
+      "gaming apparel"
+    ],
+    "featured": false,
+    "gelatoProductId": "491259c5-52b5-4e1f-899b-cde5342af48d",
+    "movement": "pokemon-logos",
+    "adminManaged": true,
+    "variants": [
+      {
+        "uid": "7428212d-a19e-4844-b10e-342c8c8ffe7a",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_xl_gco_black_gpr_4-0_gildan_64000",
+        "color": null,
+        "size": null,
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "2ccbc723-830e-4c25-96e2-ea7d2c7290c3",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_m_gco_sand_gpr_4-0_gildan_64000",
+        "color": null,
+        "size": null,
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "4e272d33-efb4-43eb-9b2d-8c4abe8b1fb4",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_l_gco_sand_gpr_4-0_gildan_64000",
+        "color": null,
+        "size": null,
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "cb6ca176-f20f-407f-b9c2-673c76be3996",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_xl_gco_sand_gpr_4-0_gildan_64000",
+        "color": null,
+        "size": null,
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "6e75096c-c4c7-4780-8717-2596242f51d3",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_l_gco_natural_gpr_4-0_gildan_64000",
+        "color": null,
+        "size": null,
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "46665566-7a7d-4fb9-8232-c965cd050901",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_s_gco_white_gpr_4-0_gildan_64000",
+        "color": null,
+        "size": null,
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "acf9b34b-cb86-4f4d-9d96-ca90221205a6",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_l_gco_light-blue_gpr_4-0_gildan_64000",
+        "color": null,
+        "size": null,
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "9ba32bd3-d1d1-4665-b775-879034131d87",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_m_gco_white_gpr_4-0_gildan_64000",
+        "color": null,
+        "size": null,
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "87d1f5b7-0e92-4497-8bcd-93a80c7d0a74",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_s_gco_light-blue_gpr_4-0_gildan_64000",
+        "color": null,
+        "size": null,
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "7484ae14-8c83-4d1a-95bf-cee13e74d4c6",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_l_gco_heather-navy_gpr_4-0_gildan_64000",
+        "color": null,
+        "size": null,
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "54eb5f74-3413-4dc9-9f0e-b5ca8096045a",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_xl_gco_heather-navy_gpr_4-0_gildan_64000",
+        "color": null,
+        "size": null,
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "02243f62-8e12-4b1f-b512-37a79a25ce24",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_xl_gco_white_gpr_4-0_gildan_64000",
+        "color": null,
+        "size": null,
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "b8934a62-e1e9-42d9-8601-03edde4bdf80",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_m_gco_black_gpr_4-0_gildan_64000",
+        "color": null,
+        "size": null,
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "6a167a06-19a1-481f-9367-6318f577c143",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_s_gco_natural_gpr_4-0_gildan_64000",
+        "color": null,
+        "size": null,
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "9955a983-e3c0-4964-a724-b1ca09eec308",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_m_gco_natural_gpr_4-0_gildan_64000",
+        "color": null,
+        "size": null,
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "2e8417f6-7da5-4a7c-9b22-fe1b26ec1db4",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_s_gco_sand_gpr_4-0_gildan_64000",
+        "color": null,
+        "size": null,
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "2d74b943-a552-4014-8662-6903826c29a3",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_l_gco_black_gpr_4-0_gildan_64000",
+        "color": null,
+        "size": null,
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "5c7ff94a-2d17-407f-bdca-edc5511af748",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_l_gco_white_gpr_4-0_gildan_64000",
+        "color": null,
+        "size": null,
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "4e6f75f5-15ef-49fd-8ec5-f98862ea7b22",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_s_gco_heather-navy_gpr_4-0_gildan_64000",
+        "color": null,
+        "size": null,
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "34c339cd-9a2c-4af1-ab06-2ce81af0d7ae",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_s_gco_black_gpr_4-0_gildan_64000",
+        "color": null,
+        "size": null,
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "bbf8fc35-dc17-4ad6-a5cb-8e4713f6bca2",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_xl_gco_light-blue_gpr_4-0_gildan_64000",
+        "color": null,
+        "size": null,
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "1040b5e0-eddc-4290-8fab-6b00d8a5cae3",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_m_gco_heather-navy_gpr_4-0_gildan_64000",
+        "color": null,
+        "size": null,
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "4be7e015-76d0-4026-86c1-a468144d3896",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_m_gco_light-blue_gpr_4-0_gildan_64000",
+        "color": null,
+        "size": null,
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "e218c540-4c04-44d8-ba70-185b9ef6c57a",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_xl_gco_natural_gpr_4-0_gildan_64000",
+        "color": null,
+        "size": null,
+        "price": null,
+        "currency": null
+      }
+    ]
+  }
+]
