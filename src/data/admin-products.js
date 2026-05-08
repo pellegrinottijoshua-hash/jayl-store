89d49952a4f95897cbd518d31f5ee623da451652
pellegrinottijoshua-hash <pellegrinottijoshua@gmail.com>
2026-05-08 17:47:10 +0200

    admin: add mewtwo-pok-mon-t-shirt-retro-90s-style-anime-gift

--- src/data/admin-products.js
+++ src/data/admin-products.js
@@ -258,5 +258,296 @@ export const adminProducts = [
         "image": "/images/zapdos-pok-mon-t-shirt/zapdos-pok-mon-t-shirt-light-blue-01.jpg"
       }
     ]
+  },
+  {
+    "id": "mewtwo-pok-mon-t-shirt-retro-90s-style-anime-gift",
+    "section": "objects",
+    "collection": "cool Pokèmon",
+    "name": "Mewtwo Pokémon T-Shirt | Retro 90s Style Anime Gift",
+    "subtitle": "cool Pokèmon",
+    "price": 1999,
+    "currency": "eur",
+    "description": "Unleash your inner Pokémon Master with our Mewtwo T-Shirt, a perfect blend of retro 90s style and cool anime fan art. Crafted from high-quality, soft cotton, this shirt features vibrant Gelato print quality that brings the iconic Mewtwo design to life. Ideal for casual outings or as a statement piece, it makes a funny and thoughtful gift for any Pokémon lover. Whether you're battling in the Pokémon arena or just hanging out with friends, this tee is a must-have for fans who appreciate unique art and nostalgia.",
+    "altText": "Mewtwo graphic t-shirt featuring retro 90s anime art style.",
+    "details": [
+      "Printed and fulfilled via Gelato"
+    ],
+    "sizes": [
+      {
+        "id": "M",
+        "label": "M",
+        "price": 1999
+      },
+      {
+        "id": "L",
+        "label": "L",
+        "price": 1999
+      },
+      {
+        "id": "S",
+        "label": "S",
+        "price": 1999
+      },
+      {
+        "id": "XL",
+        "label": "XL",
+        "price": 1999
+      }
+    ],
+    "image": "/images/mewtwo-pok-mon-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift/mewtwo-pok-mon-t-shirt-cool-anime-fan-art-gif-mockup-01.jpg",
+    "images": [
+      "/images/mewtwo-pok-mon-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift/mewtwo-pok-mon-t-shirt-cool-anime-fan-art-gif-mockup-01.jpg",
+      "/images/mewtwo-pok-mon-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift/mewtwo-pok-mon-t-shirt-cool-anime-fan-art-gif-black-01.jpg",
+      "/images/mewtwo-pok-mon-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift/mewtwo-pok-mon-t-shirt-cool-anime-fan-art-gif-navy-01.jpg",
+      "/images/mewtwo-pok-mon-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift/mewtwo-pok-mon-t-shirt-cool-anime-fan-art-gif-heather-navy-01.jpg",
+      "/images/mewtwo-pok-mon-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift/mewtwo-pok-mon-t-shirt-cool-anime-fan-art-gif-purple-01.jpg",
+      "/images/mewtwo-pok-mon-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift/mewtwo-pok-mon-t-shirt-cool-anime-fan-art-gif-white-01.jpg"
+    ],
+    "imageAlts": {
+      "/images/mewtwo-pok-mon-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift/mewtwo-pok-mon-t-shirt-cool-anime-fan-art-gif-mockup-01.jpg": "Mewtwo graphic t-shirt featuring retro 90s anime art style.",
+      "/images/mewtwo-pok-mon-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift/mewtwo-pok-mon-t-shirt-cool-anime-fan-art-gif-black-01.jpg": "Mewtwo graphic t-shirt featuring retro 90s anime art style.",
+      "/images/mewtwo-pok-mon-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift/mewtwo-pok-mon-t-shirt-cool-anime-fan-art-gif-navy-01.jpg": "Mewtwo graphic t-shirt featuring retro 90s anime art style.",
+      "/images/mewtwo-pok-mon-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift/mewtwo-pok-mon-t-shirt-cool-anime-fan-art-gif-heather-navy-01.jpg": "Mewtwo graphic t-shirt featuring retro 90s anime art style.",
+      "/images/mewtwo-pok-mon-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift/mewtwo-pok-mon-t-shirt-cool-anime-fan-art-gif-purple-01.jpg": "Mewtwo graphic t-shirt featuring retro 90s anime art style.",
+      "/images/mewtwo-pok-mon-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift/mewtwo-pok-mon-t-shirt-cool-anime-fan-art-gif-white-01.jpg": "Mewtwo graphic t-shirt featuring retro 90s anime art style."
+    },
+    "tags": [
+      "mewtwo t-shirt",
+      "pokemon gift",
+      "anime fan art",
+      "90s style shirt",
+      "funny pokemon tee",
+      "retro anime shirt",
+      "pokemon lover gift",
+      "cool graphic tee",
+      "anime merchandise",
+      "geek gifts",
+      "cotton t-shirt",
+      "vintage pokemon",
+      "artistic apparel"
+    ],
+    "featured": false,
+    "gelatoProductId": "0edda569-9ce8-43a0-9475-ead1cd2919b5",
+    "movement": "cool Pokèmon",
+    "adminManaged": true,
+    "primaryKeywords": [
+      "mewtwo t-shirt",
+      "pokemon shirt",
+      "anime t-shirt",
+      "retro graphic tee",
+      "funny anime gift"
+    ],
+    "longTailKeywords": [
+      "mewtwo pokemon t-shirt",
+      "retro 90s anime shirt",
+      "funny pokemon lover gift",
+      "cool graphic t-shirt for fans",
+      "soft cotton anime tee",
+      "pokemon fan art shirt",
+      "vintage style graphic tee",
+      "unique anime merchandise",
+      "mewtwo art t-shirt",
+      "gift for pokemon fans"
+    ],
+    "hashtags": "#mewtwo #pokemongift #animetee #retrovintage #geekwear #pokemonlover #fanart #90sstyle #animefan #graphictee #artapparel #cottonshirt #vintageanime #funnytshirt #fandomfashion #animecollection #geekgifts #nostalgia #cooltshirt #uniquegifts #artlovers #pokemontshirt #animeart #retrostyle #fanmerch #artisticfashion #geekculture #animecommunity #collectibles #mewtwoart #pokemonthemed",
+    "instagramCaption": "Level up your wardrobe with this Mewtwo tee! Perfect for Pokémon battles or casual hangouts, it’s a must-have for any fan. Grab yours now! #mewtwo #pokemonlover #animetee",
+    "pinterestCaption": "Discover our Mewtwo Pokémon T-Shirt, a retro 90s style piece that every anime fan will love. Made from high-quality cotton with stunning Gelato print quality, this shirt is perfect for casual wear or as a unique gift. Don't miss out on adding this iconic design to your collection!",
+    "variants": [
+      {
+        "uid": "304969d7-880f-402c-87aa-7eff1e5f684c",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_m_gco_purple_gpr_4-0_inlbl_gildan_64000",
+        "color": "Purple",
+        "size": "M",
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "e7af6d7e-ea7c-401c-8ba3-50c29987b909",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_l_gco_black_gpr_4-0_inlbl_gildan_64000",
+        "color": "Black",
+        "size": "L",
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "3d906a33-c7d7-4efa-93fd-ef7998c0a9e3",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_s_gco_black_gpr_4-0_inlbl_gildan_64000",
+        "color": "Black",
+        "size": "S",
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "bda9a01f-d7b1-4fe5-92f9-005c546ef6a1",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_l_gco_white_gpr_4-0_inlbl_gildan_64000",
+        "color": "White",
+        "size": "L",
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "bf7bb6da-af3f-4a86-af38-bb775a1eec34",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_xl_gco_navy_gpr_4-0_inlbl_gildan_64000",
+        "color": "Navy",
+        "size": "XL",
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "460af9b3-c5bb-43e0-bda3-6a459c8bc41b",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_s_gco_white_gpr_4-0_inlbl_gildan_64000",
+        "color": "White",
+        "size": "S",
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "d0eddc85-17a2-4dfc-98c6-d91a68d753f1",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_m_gco_white_gpr_4-0_inlbl_gildan_64000",
+        "color": "White",
+        "size": "M",
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "fe3b420d-1c26-4a30-9258-449e9c0669c5",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_m_gco_navy_gpr_4-0_inlbl_gildan_64000",
+        "color": "Navy",
+        "size": "M",
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "8b87bf7c-c68b-4e1b-adbb-77c390e3744a",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_s_gco_navy_gpr_4-0_inlbl_gildan_64000",
+        "color": "Navy",
+        "size": "S",
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "abd4813e-0542-4844-aacd-8d826540c365",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_m_gco_heather-navy_gpr_4-0_inlbl_gildan_64000",
+        "color": "Heather Navy",
+        "size": "M",
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "f5b1e5ad-1ba0-4241-9609-8a2dd02f7f06",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_xl_gco_white_gpr_4-0_inlbl_gildan_64000",
+        "color": "White",
+        "size": "XL",
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "0f2dec02-4402-461d-9578-c26987efdfb6",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_s_gco_heather-navy_gpr_4-0_inlbl_gildan_64000",
+        "color": "Heather Navy",
+        "size": "S",
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "ae595959-1038-41f4-a571-efeeda33d97f",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_l_gco_heather-navy_gpr_4-0_inlbl_gildan_64000",
+        "color": "Heather Navy",
+        "size": "L",
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "92d4c8dc-fd25-499d-92d5-7800cca8f31a",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_m_gco_black_gpr_4-0_inlbl_gildan_64000",
+        "color": "Black",
+        "size": "M",
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "47962563-26c8-4c76-9199-d8e9af565540",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_xl_gco_black_gpr_4-0_inlbl_gildan_64000",
+        "color": "Black",
+        "size": "XL",
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "28c2cb06-f8c6-4b72-a447-babdd318bf73",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_xl_gco_purple_gpr_4-0_inlbl_gildan_64000",
+        "color": "Purple",
+        "size": "XL",
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "01bc5c69-afa5-4565-8c9d-c21afc7ba02f",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_l_gco_purple_gpr_4-0_inlbl_gildan_64000",
+        "color": "Purple",
+        "size": "L",
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "b4610e82-45f4-40c7-bbdd-eb4b2ef96cbd",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_s_gco_purple_gpr_4-0_inlbl_gildan_64000",
+        "color": "Purple",
+        "size": "S",
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "e86c1a63-7c2c-4784-9d75-8aedd2867e30",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_xl_gco_heather-navy_gpr_4-0_inlbl_gildan_64000",
+        "color": "Heather Navy",
+        "size": "XL",
+        "price": null,
+        "currency": null
+      },
+      {
+        "uid": "455ec782-08df-4e7b-a4fb-13e89405d792",
+        "gelatoVariantId": "apparel_product_gca_t-shirt_gsc_crewneck_gcu_unisex_gqa_classic_gsi_l_gco_navy_gpr_4-0_inlbl_gildan_64000",
+        "color": "Navy",
+        "size": "L",
+        "price": null,
+        "currency": null
+      }
+    ],
+    "colors": [
+      {
+        "id": "purple",
+        "label": "Purple",
+        "hex": "#888888",
+        "image": "/images/mewtwo-pok-mon-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift/mewtwo-pok-mon-t-shirt-cool-anime-fan-art-gif-purple-01.jpg"
+      },
+      {
+        "id": "black",
+        "label": "Black",
+        "hex": "#888888",
+        "image": "/images/mewtwo-pok-mon-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift/mewtwo-pok-mon-t-shirt-cool-anime-fan-art-gif-black-01.jpg"
+      },
+      {
+        "id": "white",
+        "label": "White",
+        "hex": "#888888",
+        "image": "/images/mewtwo-pok-mon-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift/mewtwo-pok-mon-t-shirt-cool-anime-fan-art-gif-white-01.jpg"
+      },
+      {
+        "id": "navy",
+        "label": "Navy",
+        "hex": "#888888",
+        "image": "/images/mewtwo-pok-mon-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift/mewtwo-pok-mon-t-shirt-cool-anime-fan-art-gif-navy-01.jpg"
+      },
+      {
+        "id": "heather-navy",
+        "label": "Heather Navy",
+        "hex": "#888888",
+        "image": "/images/mewtwo-pok-mon-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift/mewtwo-pok-mon-t-shirt-cool-anime-fan-art-gif-heather-navy-01.jpg"
+      }
+    ],
+    "printCost": 700,
+    "createdAt": "2026-05-08T15:47:10.772Z",
+    "updatedAt": "2026-05-08T15:47:10.772Z"
   }
 ]
