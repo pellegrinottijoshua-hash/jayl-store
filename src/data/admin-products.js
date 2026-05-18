d0ffcc50297b86373494cfc3f63327abced49bf0
pellegrinottijoshua-hash <pellegrinottijoshua@gmail.com>
2026-05-18 22:18:33 +0200

    admin: update dragonite-pok-mon-t-shirt-retro-90s-style-gift

--- src/data/admin-products.js
+++ src/data/admin-products.js
@@ -297,7 +297,7 @@ export const adminProducts = [
     "id": "dragonite-pok-mon-t-shirt-retro-90s-style-gift",
     "section": "objects",
     "collection": "cool Pokèmon",
-    "name": "Dragonite Pokémon T-Shirt",
+    "name": "Cool Dragonite T-Shirt",
     "subtitle": "cool Pokèmon",
     "price": 1999,
     "currency": "eur",
@@ -584,7 +584,7 @@ export const adminProducts = [
     ],
     "printCost": 700,
     "createdAt": "2026-05-09T15:58:25.116Z",
-    "updatedAt": "2026-05-18T19:46:04.080Z",
+    "updatedAt": "2026-05-18T20:18:32.913Z",
     "heroImage": "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/images/dragonite-pok-mon-t-shirt-retro-90s-style-gift/hf_20260518_140959_a8359d45-7e06-4262-8f4c-b12b30329431.jpg",
     "relatedProducts": []
   },
