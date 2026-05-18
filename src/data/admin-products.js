b00c41154aebb038522f0a6dd67147df906f28ed
pellegrinottijoshua-hash <pellegrinottijoshua@gmail.com>
2026-05-18 21:28:46 +0200

    admin: update psyduck-pok-mon-t-shirt-retro-90s-style-anime-gift

--- src/data/admin-products.js
+++ src/data/admin-products.js
@@ -1455,8 +1455,9 @@ export const adminProducts = [
       }
     ],
     "createdAt": "2026-05-09T16:39:15.748Z",
-    "updatedAt": "2026-05-18T19:28:40.040Z",
-    "heroImage": "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/images/psyduck-pok-mon-t-shirt-retro-90s-style-anime-gift/hf_20260518_140302_d6319253-126b-4663-8c12-4f886a1da22b.jpg"
+    "updatedAt": "2026-05-18T19:28:46.822Z",
+    "heroImage": "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/images/psyduck-pok-mon-t-shirt-retro-90s-style-anime-gift/hf_20260518_140302_d6319253-126b-4663-8c12-4f886a1da22b.jpg",
+    "relatedProducts": []
   },
   {
     "id": "charizard-pok-mon-t-shirt-gildan-9400-premium-artwear",
