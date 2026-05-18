ce836db4334be419cfeba90d9dad748a976ff75d
pellegrinottijoshua-hash <pellegrinottijoshua@gmail.com>
2026-05-18 22:12:32 +0200

    admin: update snorlax-pok-mon-t-shirt

--- src/data/admin-products.js
+++ src/data/admin-products.js
@@ -1167,7 +1167,7 @@ export const adminProducts = [
       }
     ],
     "createdAt": "2026-05-09T16:32:03.324Z",
-    "updatedAt": "2026-05-18T20:12:28.599Z",
+    "updatedAt": "2026-05-18T20:12:32.673Z",
     "heroImage": "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/images/snorlax-pok-mon-t-shirt/hf_20260518_183826_bacc4315-c54a-4661-8044-673ab5556382.jpg",
     "relatedProducts": []
   },
