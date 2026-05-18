7e0d14ef1f88cd4370b0335a30193e991035f04e
pellegrinottijoshua-hash <pellegrinottijoshua@gmail.com>
2026-05-18 15:56:06 +0200

    admin: feature charizard-pok-mon-t-shirt-gildan-9400-premium-artwear

--- src/data/admin-products.js
+++ src/data/admin-products.js
@@ -1524,7 +1524,7 @@ export const adminProducts = [
       "anime fan gear",
       "casual wear"
     ],
-    "featured": false,
+    "featured": true,
     "gelatoProductId": "6088cc0d-4c75-4194-abf4-ac5a472c1d32",
     "movement": "cool Pokèmon",
     "adminManaged": true,
@@ -1745,7 +1745,7 @@ export const adminProducts = [
       }
     ],
     "createdAt": "2026-05-14T15:23:15.291Z",
-    "updatedAt": "2026-05-18T12:41:48.587Z",
+    "updatedAt": "2026-05-18T13:56:06.283Z",
     "heroImage": "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/images/charizard-pok-mon-t-shirt-gildan-9400-premium-artwear/hf_20260514_163644_b595e5c4-10e4-4059-bcf9-2b787f7b2c37-1.jpg"
   }
 ]
