b80ef16b8a801dfa5bb4713e0dc19811e34c5b2b
pellegrinottijoshua-hash <pellegrinottijoshua@gmail.com>
2026-05-22 00:26:47 +0200

    admin: update dragonite-pok-mon-t-shirt-retro-90s-style-gift

--- src/data/admin-products.js
+++ src/data/admin-products.js
@@ -584,7 +584,7 @@ export const adminProducts = [
     ],
     "printCost": 700,
     "createdAt": "2026-05-09T15:58:25.116Z",
-    "updatedAt": "2026-05-18T20:18:32.913Z",
+    "updatedAt": "2026-05-21T22:26:47.265Z",
     "heroImage": "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/images/dragonite-pok-mon-t-shirt-retro-90s-style-gift/hf_20260518_140959_a8359d45-7e06-4262-8f4c-b12b30329431.jpg",
     "relatedProducts": []
   },
