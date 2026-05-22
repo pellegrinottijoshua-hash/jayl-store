9d67923c85f7b8dd059dd19b067d0d3e03bd8e3a
pellegrinottijoshua-hash <pellegrinottijoshua@gmail.com>
2026-05-22 03:12:09 +0200

    admin: update images order for snorlax-pok-mon-t-shirt

--- src/data/admin-products.js
+++ src/data/admin-products.js
@@ -1168,9 +1168,10 @@ export const adminProducts = [
       }
     ],
     "createdAt": "2026-05-09T16:32:03.324Z",
-    "updatedAt": "2026-05-22T01:10:07.829Z",
+    "updatedAt": "2026-05-22T01:12:09.121Z",
     "heroImage": "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/images/snorlax-pok-mon-t-shirt/hf_20260518_183826_bacc4315-c54a-4661-8044-673ab5556382.jpg",
-    "relatedProducts": []
+    "relatedProducts": [],
+    "detailImage": "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/images/snorlax-pok-mon-t-shirt/hf_20260522_010620_5a00065b-76e1-4b6f-b4c3-59475c678acb.jpg"
   },
   {
     "id": "psyduck-pok-mon-t-shirt-retro-90s-style-anime-gift",
