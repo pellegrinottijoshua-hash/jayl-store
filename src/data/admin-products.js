58b7890621a22b34d783106b1be6d148031ece6b
pellegrinottijoshua-hash <pellegrinottijoshua@gmail.com>
2026-05-22 03:26:45 +0200

    admin: update psyduck-pok-mon-t-shirt-retro-90s-style-anime-gift

--- src/data/admin-products.js
+++ src/data/admin-products.js
@@ -1464,7 +1464,7 @@ export const adminProducts = [
       }
     ],
     "createdAt": "2026-05-09T16:39:15.748Z",
-    "updatedAt": "2026-05-22T01:20:55.294Z",
+    "updatedAt": "2026-05-22T01:26:45.006Z",
     "heroImage": "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/images/psyduck-pok-mon-t-shirt-retro-90s-style-anime-gift/hf_20260518_140302_d6319253-126b-4663-8c12-4f886a1da22b.jpg",
     "relatedProducts": [],
     "detailImage": "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/images/psyduck-pok-mon-t-shirt-retro-90s-style-anime-gift/hf_20260509_235330_df7490f5-6f53-4f84-8e4f-c7820cef98df.jpg"
