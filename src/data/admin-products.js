be4044c7d24430366e22a2704355acb53bcbef51
pellegrinottijoshua-hash <pellegrinottijoshua@gmail.com>
2026-05-22 03:17:31 +0200

    admin: update mewtwo-pok-mon-t-shirt-retro-90s-style-anime-gift

--- src/data/admin-products.js
+++ src/data/admin-products.js
@@ -289,7 +289,7 @@ export const adminProducts = [
     ],
     "printCost": 700,
     "createdAt": "2026-05-08T15:47:10.772Z",
-    "updatedAt": "2026-05-22T01:17:29.063Z",
+    "updatedAt": "2026-05-22T01:17:31.165Z",
     "heroImage": "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/images/mewtwo-pok-mon-t-shirt-retro-90s-style-anime-gift/hf_20260518_182947_a31835ef-1315-4672-95f2-2697b92ee003.jpg",
     "relatedProducts": [],
     "detailImage": "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/images/mewtwo-pok-mon-t-shirt-retro-90s-style-anime-gift/hf_20260522_011343_a643d839-7cbe-4c09-b7fa-a2d74d1a8ee8.jpg"
