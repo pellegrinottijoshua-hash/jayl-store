b0c576965463d70465a545f562bdec81d7922b09
pellegrinottijoshua-hash <pellegrinottijoshua@gmail.com>
2026-05-23 18:47:20 +0200

    admin: update cool-vileploom-pok-mon-t-shirt

--- src/data/admin-products.js
+++ src/data/admin-products.js
@@ -2110,7 +2110,7 @@ export const adminProducts = [
     ],
     "printCost": 700,
     "createdAt": "2026-05-23T16:18:25.070Z",
-    "updatedAt": "2026-05-23T16:45:36.510Z",
+    "updatedAt": "2026-05-23T16:47:20.114Z",
     "relatedProducts": [],
     "detailImage": "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/images/cool-vileploom-pok-mon-t-shirt/hf_20260523_161159_50a0ea14-0e69-4dd4-98e7-bbfbbd685ffe.jpg"
   }
