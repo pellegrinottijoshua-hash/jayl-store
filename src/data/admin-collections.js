ee52d636c35828bb794ffc6fe3372bee0ff54e7c
pellegrinottijoshua-hash <pellegrinottijoshua@gmail.com>
2026-06-26 13:02:42 +0200

    admin: update collection cool-pok-mon

--- src/data/admin-collections.js
+++ src/data/admin-collections.js
@@ -2,7 +2,7 @@
 export const adminCollections = [
   {
     "id": "cool-pok-mon",
-    "name": "cool pokémon (front)",
+    "name": "Cool Pokémon (front)",
     "color": "#f2be64",
     "coverImage": "",
     "order": 0
