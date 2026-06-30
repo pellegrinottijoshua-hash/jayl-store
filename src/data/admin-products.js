1650a02527fc1559e59db3c43477543cf1149f1f
pellegrinottijoshua-hash <pellegrinottijoshua@gmail.com>
2026-06-30 17:02:09 +0200

    admin: update gengar-pok-mon-back-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift-gildan-64000

--- src/data/admin-products.js
+++ src/data/admin-products.js
@@ -11339,7 +11339,7 @@ export const adminProducts = [
       "https://gelato-api-live.s3.eu-west-1.amazonaws.com/ecommerce/store_product_image/e80c69b9-3c96-413c-916c-4dba2db14a53/preview?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA3QM3COBP5QN4IUGA%2F20260630%2Feu-west-1%2Fs3%2Faws4_request&X-Amz-Date=20260630T150138Z&X-Amz-SignedHeaders=host&X-Amz-Expires=86400&X-Amz-Signature=7dd8fcc6dda4e9a0e9c6b959fb87f0e2001239b37bbddabd015418629c08c2dc"
     ],
     "createdAt": "2026-06-29T12:52:31.776Z",
-    "updatedAt": "2026-06-30T15:02:00.562Z",
+    "updatedAt": "2026-06-30T15:02:09.684Z",
     "relatedProducts": []
   },
   {
