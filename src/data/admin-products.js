4ea8de7bc21de28ebb645a26c311a64db9dd0bb4
pellegrinottijoshua-hash <pellegrinottijoshua@gmail.com>
2026-07-02 16:21:57 +0200

    admin: update cool-snorlax-back-t-shirt

--- src/data/admin-products.js
+++ src/data/admin-products.js
@@ -7676,7 +7676,7 @@ export const adminProducts = [
       "https://gelato-api-live.s3.eu-west-1.amazonaws.com/ecommerce/store_product_image/88c51c10-06b9-4d81-ac8b-ea49d23d9db2/preview?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA3QM3COBP5QN4IUGA%2F20260626%2Feu-west-1%2Fs3%2Faws4_request&X-Amz-Date=20260626T131507Z&X-Amz-SignedHeaders=host&X-Amz-Expires=86400&X-Amz-Signature=555d6961abfa6a2a033c395311bfcb20e33411f568c31df3700e190c5445fb05"
     ],
     "createdAt": "2026-06-26T13:25:03.416Z",
-    "updatedAt": "2026-07-02T14:12:47.366Z",
+    "updatedAt": "2026-07-02T14:21:57.308Z",
     "printFileUrl": "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/designs/cool-snorlax-back-t-shirt/design.png",
     "relatedProducts": [],
     "pinterestPins": [
@@ -7744,7 +7744,8 @@ export const adminProducts = [
     ],
     "pinterestPublishedImages": [
       "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/images/cool-snorlax-back-t-shirt/hf_20260626_121424_593e8fb3-bb55-43db-bb2e-258b6376819a.jpeg"
-    ]
+    ],
+    "neckLabelUrl": "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/designs/jayl-neck-label.svg"
   },
   {
     "id": "cool-vileplume-back-t-shirt",
