7181df212bf17356cdb7fc003e4d3a60e5c9df03
pellegrinottijoshua-hash <pellegrinottijoshua@gmail.com>
2026-07-02 16:03:54 +0200

    admin: update cool-alakazam-back-t-shirt

--- src/data/admin-products.js
+++ src/data/admin-products.js
@@ -9165,7 +9165,7 @@ export const adminProducts = [
       "https://gelato-api-live.s3.eu-west-1.amazonaws.com/ecommerce/store_product_image/1bf5482b-d85d-4f96-964f-211cb4aeb01c/preview?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA3QM3COBP5QN4IUGA%2F20260626%2Feu-west-1%2Fs3%2Faws4_request&X-Amz-Date=20260626T161532Z&X-Amz-SignedHeaders=host&X-Amz-Expires=86400&X-Amz-Signature=08c18def25e89a934e05186ef5aa825399b9f2313152ed86eafa74afe3ac093f"
     ],
     "createdAt": "2026-06-26T16:31:53.176Z",
-    "updatedAt": "2026-07-02T14:03:36.941Z",
+    "updatedAt": "2026-07-02T14:03:54.037Z",
     "printFileUrl": "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/designs/cool-alakazam-back-t-shirt/design.png",
     "relatedProducts": [],
     "pinterestPins": [
@@ -9179,7 +9179,8 @@ export const adminProducts = [
           "t-shirt",
           "cool style",
           "unique design"
-        ]
+        ],
+        "published": true
       },
       {
         "title": "Gift the Ultimate Pokémon Fan: Cool Alakazam T-Shirt",
@@ -9229,6 +9230,9 @@ export const adminProducts = [
           "fan apparel"
         ]
       }
+    ],
+    "pinterestPublishedImages": [
+      "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/images/cool-alakazam-back-t-shirt/hf_20260626_161220_0af2ad1d-118b-4a78-8a5f-87420da4eed8.png"
     ]
   },
   {
