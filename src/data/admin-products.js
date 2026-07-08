41eb006033990bf7ba70921906cd37970acdca67
pellegrinottijoshua-hash <pellegrinottijoshua@gmail.com>
2026-07-08 17:20:10 +0200

    admin: update cool-drowsee-back-t-shirt

--- src/data/admin-products.js
+++ src/data/admin-products.js
@@ -12606,7 +12606,7 @@ export const adminProducts = [
       "https://gelato-api-live.s3.eu-west-1.amazonaws.com/ecommerce/store_product_image/d3682315-ca68-433a-9cb8-12e9f90f9f1f/preview?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA3QM3COBP5QN4IUGA%2F20260627%2Feu-west-1%2Fs3%2Faws4_request&X-Amz-Date=20260627T134759Z&X-Amz-SignedHeaders=host&X-Amz-Expires=86400&X-Amz-Signature=52d2a0a9bfa536c89187ccec429a1bcafda95aa5e39286f9241ced3bc8cf1294"
     ],
     "createdAt": "2026-06-27T14:19:42.734Z",
-    "updatedAt": "2026-07-02T14:19:39.374Z",
+    "updatedAt": "2026-07-08T15:20:10.342Z",
     "printFileUrl": "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/designs/cool-drowsee-back-t-shirt/design.png",
     "relatedProducts": [],
     "pinterestPins": [
@@ -12633,7 +12633,8 @@ export const adminProducts = [
           "apparel",
           "outfit",
           "fashion"
-        ]
+        ],
+        "published": true
       },
       {
         "title": "The Perfect Gift for Pokémon Lovers: Cool Drowsee Tee",
@@ -12674,7 +12675,8 @@ export const adminProducts = [
     ],
     "pinterestPublishedImages": [
       "https://gelato-api-live.s3.eu-west-1.amazonaws.com/ecommerce/store_product_image/9bb0f4ef-4317-4b3a-a2a1-51952db076c8/preview?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA3QM3COBP5QN4IUGA%2F20260627%2Feu-west-1%2Fs3%2Faws4_request&X-Amz-Date=20260627T134759Z&X-Amz-SignedHeaders=host&X-Amz-Expires=86400&X-Amz-Signature=fdb17f259cc556263b59b4389e5befe788d89f46e6b365de85548b621bff324f",
-      "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/images/cool-drowsee-back-t-shirt/hf_20260627_141308_14f8cae7-0231-48c8-bf1a-fbdeed17d579.png"
+      "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/images/cool-drowsee-back-t-shirt/hf_20260627_141308_14f8cae7-0231-48c8-bf1a-fbdeed17d579.png",
+      "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/images/cool-drowsee-back-t-shirt/hf_20260627_135307_82146959-9e93-4c4c-8763-98e117a21a56.png"
     ],
     "neckLabelUrl": "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/designs/jayl-neck-label.svg"
   },
