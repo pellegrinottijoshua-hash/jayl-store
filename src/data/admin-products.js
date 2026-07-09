08c4a019239bbbafb7fdc8fa074571eb0a722518
pellegrinottijoshua-hash <pellegrinottijoshua@gmail.com>
2026-07-09 15:34:29 +0200

    admin: update mewtwo-pok-mon-t-shirt-retro-90s-style-anime-gift

--- src/data/admin-products.js
+++ src/data/admin-products.js
@@ -303,7 +303,7 @@ export const adminProducts = [
     ],
     "printCost": 700,
     "createdAt": "2026-05-08T15:47:10.772Z",
-    "updatedAt": "2026-07-03T11:54:27.383Z",
+    "updatedAt": "2026-07-09T13:34:28.894Z",
     "heroImage": "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/images/mewtwo-pok-mon-t-shirt-retro-90s-style-anime-gift/hf_20260518_182947_a31835ef-1315-4672-95f2-2697b92ee003.jpg",
     "relatedProducts": [],
     "detailImage": "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/images/mewtwo-pok-mon-t-shirt-retro-90s-style-anime-gift/hf_20260522_011343_a643d839-7cbe-4c09-b7fa-a2d74d1a8ee8.jpg",
@@ -344,7 +344,8 @@ export const adminProducts = [
           "fandom fashion",
           "casual wear",
           "gift ideas"
-        ]
+        ],
+        "published": true
       },
       {
         "title": "The Perfect Gift for Pokémon Lovers: Cool Mewtwo T-Shirt",
@@ -397,7 +398,8 @@ export const adminProducts = [
       }
     ],
     "pinterestPublishedImages": [
-      "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/images/mewtwo-pok-mon-t-shirt-retro-90s-style-anime-gift/hf_20260511_163805_225351ce-e688-4f67-bdf7-7e8c952cf5cf.jpg"
+      "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/images/mewtwo-pok-mon-t-shirt-retro-90s-style-anime-gift/hf_20260511_163805_225351ce-e688-4f67-bdf7-7e8c952cf5cf.jpg",
+      "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/images/mewtwo-pok-mon-t-shirt-retro-90s-style-anime-gift/hf_20260522_011343_a643d839-7cbe-4c09-b7fa-a2d74d1a8ee8.jpg"
     ]
   },
   {
