efcb50f11718e9821c1031157d773834a5eb0d6e
pellegrinottijoshua-hash <pellegrinottijoshua@gmail.com>
2026-07-15 15:21:05 +0200

    admin: update cool-snorlax-back-t-shirt

--- src/data/admin-products.js
+++ src/data/admin-products.js
@@ -9079,7 +9079,7 @@ export const adminProducts = [
       "https://gelato-api-live.s3.eu-west-1.amazonaws.com/ecommerce/store_product_image/88c51c10-06b9-4d81-ac8b-ea49d23d9db2/preview?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA3QM3COBP5QN4IUGA%2F20260626%2Feu-west-1%2Fs3%2Faws4_request&X-Amz-Date=20260626T131507Z&X-Amz-SignedHeaders=host&X-Amz-Expires=86400&X-Amz-Signature=555d6961abfa6a2a033c395311bfcb20e33411f568c31df3700e190c5445fb05"
     ],
     "createdAt": "2026-06-26T13:25:03.416Z",
-    "updatedAt": "2026-07-07T13:24:08.500Z",
+    "updatedAt": "2026-07-15T13:21:05.098Z",
     "printFileUrl": "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/designs/cool-snorlax-back-t-shirt/design.png",
     "relatedProducts": [],
     "pinterestPins": [
@@ -9119,7 +9119,8 @@ export const adminProducts = [
           "casual wear",
           "fashion",
           "graphic tee"
-        ]
+        ],
+        "published": true
       },
       {
         "title": "Stand Out at Events with the Cool Snorlax T-Shirt",
@@ -9148,7 +9149,8 @@ export const adminProducts = [
     ],
     "pinterestPublishedImages": [
       "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/images/cool-snorlax-back-t-shirt/hf_20260626_121424_593e8fb3-bb55-43db-bb2e-258b6376819a.jpeg",
-      "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/images/cool-snorlax-back-t-shirt/hf_20260626_114025_c69996c2-a7f5-418c-99c4-d61a7b9f8f03.jpeg"
+      "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/images/cool-snorlax-back-t-shirt/hf_20260626_114025_c69996c2-a7f5-418c-99c4-d61a7b9f8f03.jpeg",
+      "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/images/cool-snorlax-back-t-shirt/hf_20260626_121455_9160de3b-30c7-49ea-996d-85a0eebc2d36.png"
     ],
     "neckLabelUrl": "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/designs/jayl-neck-label.svg"
   },
