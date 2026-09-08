c936660e95e94a1de0472dafc02f428d2e0f91f9
pellegrinottijoshua-hash <pellegrinottijoshua@gmail.com>
2026-09-08 16:56:32 +0200

    admin: update raikou-back-print-shirt-funny-retro-90s-anime-graphic-tee-large-back-design-unisex-cotton-t-shirt-electric-pokemon-gift-for-him

--- src/data/admin-products.js
+++ src/data/admin-products.js
@@ -17011,7 +17011,7 @@ export const adminProducts = [
     "printFileUrl": "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/designs/raikou-back-print-shirt-funny-retro-90s-anime-graphic-tee-large-back-design-unisex-cotton-t-shirt-electric-pokemon-gift-for-him/hf_20260816_113839_3651409f-d368-400f-9dbf-72a8d002ff8a-gelato-front-350.png",
     "neckLabelUrl": "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/designs/jayl-neck-label.png",
     "createdAt": "2026-09-08T14:47:54.039Z",
-    "updatedAt": "2026-09-08T14:50:47.044Z",
+    "updatedAt": "2026-09-08T14:56:31.828Z",
     "relatedProducts": []
   }
 ]
