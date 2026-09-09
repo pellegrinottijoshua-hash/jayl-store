9918c031bff14ae14e507ec7c767be02d5599d9c
pellegrinottijoshua-hash <pellegrinottijoshua@gmail.com>
2026-09-09 18:02:39 +0200

    [drop] update drop-01-sleep-mode

--- src/data/drop.js
+++ src/data/drop.js
@@ -24,7 +24,7 @@ export const drop = {
       "psyduck-pok-mon-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift-gildan-64000"
     ],
     "startsAt": "2026-09-06T16:00:00Z",
-    "endsAt": "2026-09-09T16:00:00Z",
+    "endsAt": "2026-09-10T16:00:00Z",
     "cap": 20,
     "caps": {},
     "dropPrice": 2200,
@@ -46,14 +46,16 @@ export const drop = {
         "altaria-back-print-shirt-funny-retro-90s-anime-graphic-tee-large-back-design-unisex-cotton-t-shirt-dragon-pokemon-gift-for-him",
         "ursaring-back-print-shirt-funny-retro-90s-anime-graphic-tee-large-back-design-unisex-cotton-t-shirt-pokemon-gift-for-him"
       ],
-      "startsAt": "2026-09-09T16:00:00Z",
-      "endsAt": "2026-09-13T16:00:00Z",
+      "startsAt": "2026-09-10T16:00:00Z",
+      "endsAt": "2026-09-14T16:00:00Z",
       "cap": 20,
       "caps": {},
       "dropPrice": 2200,
       "bundlePrice": 5700,
       "heroImages": {
-        "cool-snorlax-back-t-shirt": "/images/cool-snorlax-back-t-shirt/s-post-1.png"
+        "cool-snorlax-back-t-shirt": "/images/cool-snorlax-back-t-shirt/s-post-1.png",
+        "altaria-back-print-shirt-funny-retro-90s-anime-graphic-tee-large-back-design-unisex-cotton-t-shirt-dragon-pokemon-gift-for-him": "/images/altaria-back-print-shirt-funny-retro-90s-anime-graphic-tee-large-back-design-unisex-cotton-t-shirt-dragon-pokemon-gift-for-him/hf_20260902_160750_689c4c08-c1db-4855-8612-24c038f06a38.png",
+        "ursaring-back-print-shirt-funny-retro-90s-anime-graphic-tee-large-back-design-unisex-cotton-t-shirt-pokemon-gift-for-him": "/images/ursaring-back-print-shirt-funny-retro-90s-anime-graphic-tee-large-back-design-unisex-cotton-t-shirt-pokemon-gift-for-him/u-post1.png"
       }
     }
   ],
