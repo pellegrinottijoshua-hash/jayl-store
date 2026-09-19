b2c42b9238c05dbcdb108e8b7c660f7f097c52f9
pellegrinottijoshua-hash <pellegrinottijoshua@gmail.com>
2026-09-19 14:25:28 +0200

    [drop] update drop-03

--- src/data/drop.js
+++ src/data/drop.js
@@ -70,7 +70,29 @@ export const drop = {
       "endsAt": "2026-09-13T16:00:00Z"
     }
   ],
-  "scheduled": [],
+  "scheduled": [
+    {
+      "id": "drop-04",
+      "number": 4,
+      "title": "ICONIC",
+      "productIds": [
+        "cool-arcanine-back-shirt",
+        "blastoise-pok-mon-back-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift-gildan-64000",
+        "cool-vileplume-back-t-shirt"
+      ],
+      "startsAt": "2026-09-19T16:00:00Z",
+      "endsAt": "2026-09-22T16:00:00Z",
+      "cap": 20,
+      "caps": {},
+      "dropPrice": 2200,
+      "bundlePrice": 5700,
+      "heroImages": {
+        "cool-arcanine-back-shirt": "/images/cool-arcanine-back-shirt/hf_20260904_142002_f3b2b89e-4334-4afa-8016-d2931d2d5310.png",
+        "blastoise-pok-mon-back-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift-gildan-64000": "/images/blastoise-pok-mon-back-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift-gildan-64000/hf_20260904_152245_967412b1-adc0-4cd5-8815-9b66ad07fe4a.png",
+        "cool-vileplume-back-t-shirt": "/images/cool-vileplume-back-t-shirt/hf_20260907_115923_f5785daa-be32-4143-953c-c74549e04aa4.png"
+      }
+    }
+  ],
   "next": null,
   "released": [
     "cool-mewtwo-back-t-shirt",
