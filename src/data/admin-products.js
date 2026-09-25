3d6c491412c003d4efda943da79b5ffca541658d
pellegrinottijoshua-hash <pellegrinottijoshua@gmail.com>
2026-09-25 16:34:02 +0200

    admin: update images order for cool-mewtwo-back-t-shirt

--- src/data/admin-products.js
+++ src/data/admin-products.js
@@ -9605,7 +9605,7 @@ export const adminProducts = [
       }
     ],
     "image": "/images/cool-mewtwo-back-t-shirt/hf_20260626_133224_a1409471-8ad2-48de-b617-371ce03d39ac.jpeg",
-    "heroImage": "/images/cool-mewtwo-back-t-shirt/hf_20260626_133216_9a6849e2-4c88-418a-a909-e79eb34f3647.jpeg",
+    "heroImage": "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/images/cool-mewtwo-back-t-shirt/hf_20260626_141458_7364063a-15e5-4376-aa22-4b9e4b9a8bbb.png",
     "detailImage": null,
     "images": [
       "/images/mewtwo-pok-mon-back-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift/hf_20260626_133303_3bc7245e-e51f-4504-b636-ee4060904511.png",
@@ -9901,7 +9901,7 @@ export const adminProducts = [
       "https://gelato-api-live.s3.eu-west-1.amazonaws.com/ecommerce/store_product_image/2e43137f-def8-4438-b8e1-5496065ec0ff/preview?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA3QM3COBP5QN4IUGA%2F20260626%2Feu-west-1%2Fs3%2Faws4_request&X-Amz-Date=20260626T140307Z&X-Amz-SignedHeaders=host&X-Amz-Expires=86400&X-Amz-Signature=66c9ffcfb3f4388eeeb1ddad48019f634dd2a2d90ed6aeb792857ab677bd49cb"
     ],
     "createdAt": "2026-06-26T14:15:44.480Z",
-    "updatedAt": "2026-07-15T13:24:03.457Z",
+    "updatedAt": "2026-09-25T14:34:01.891Z",
     "printFileUrl": "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/designs/cool-mewtwo-back-t-shirt/design.png",
     "relatedProducts": [],
     "pinterestPins": [
