df402b514cfe2296d3f0870d79eae148971d8080
pellegrinottijoshua-hash <pellegrinottijoshua@gmail.com>
2026-09-25 16:33:35 +0200

    admin: update cool-vileplume-back-t-shirt

--- src/data/admin-products.js
+++ src/data/admin-products.js
@@ -9195,7 +9195,7 @@ export const adminProducts = [
       }
     ],
     "image": "/images/cool-vileploom-pok-mon-back-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift-gildan-64000/hf_20260626_130147_8c434956-4fa4-4a43-b4f1-a18041f7571e.jpeg",
-    "heroImage": "/images/cool-vileploom-pok-mon-back-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift-gildan-64000/hf_20260626_125650_7848a585-778e-4179-bef1-3dfc6a3914ed.png",
+    "heroImage": "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/images/cool-vileplume-back-t-shirt/hf_20260626_125023_bbb9377a-ba52-450c-a770-27015e8ff992.jpeg",
     "detailImage": "/images/cool-vileploom-pok-mon-back-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift-gildan-64000/hf_20260626_125712_50554d29-3092-4c4e-b505-8b11189e1286.jpeg",
     "images": [
       "/images/cool-vileploom-pok-mon-back-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift-gildan-64000/hf_20260626_125732_2e0ba9e6-ad04-481d-a9a6-c5f1663427f8.jpeg",
@@ -9489,7 +9489,7 @@ export const adminProducts = [
       "https://gelato-api-live.s3.eu-west-1.amazonaws.com/ecommerce/store_product_image/c95262ca-297b-4a35-845e-62325a47a61c/preview?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA3QM3COBP5QN4IUGA%2F20260626%2Feu-west-1%2Fs3%2Faws4_request&X-Amz-Date=20260626T133143Z&X-Amz-SignedHeaders=host&X-Amz-Expires=86400&X-Amz-Signature=2e9608e1633fa1000da22966c4a28b1d27fad2261f9268becf70be82b619129a"
     ],
     "createdAt": "2026-06-26T13:41:03.363Z",
-    "updatedAt": "2026-07-15T13:23:41.332Z",
+    "updatedAt": "2026-09-25T14:33:34.880Z",
     "relatedProducts": [],
     "printFileUrl": "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/designs/cool-vileplume-back-t-shirt/design.png",
     "pinterestPins": [
