fa757e4b76aa5991a011c64c43400bc7e2fa998a
pellegrinottijoshua-hash <pellegrinottijoshua@gmail.com>
2026-09-25 16:37:51 +0200

    admin: update images order for cool-drowsee-back-t-shirt

--- src/data/admin-products.js
+++ src/data/admin-products.js
@@ -12378,7 +12378,7 @@ export const adminProducts = [
       }
     ],
     "image": "/images/drowsee-pok-mon-back-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift-gildan-64000/hf_20260627_134918_c642db58-d7d7-487f-82f5-eabd76c7f7de.png",
-    "heroImage": "/images/drowsee-pok-mon-back-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift-gildan-64000/hf_20260627_141308_14f8cae7-0231-48c8-bf1a-fbdeed17d579.png",
+    "heroImage": "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/images/cool-drowsee-back-t-shirt/hf_20260627_135244_5755ebaf-9624-411f-be56-15a628068808.png",
     "detailImage": "/images/drowsee-pok-mon-back-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift-gildan-64000/hf_20260627_135854_54f5f780-5ddd-4293-a58d-cdfea1c86695.png",
     "images": [
       "/images/drowsee-pok-mon-back-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift-gildan-64000/hf_20260627_135836_1c1a6c7d-7602-4fd4-8238-1da5a96b097e.png",
@@ -12671,7 +12671,7 @@ export const adminProducts = [
       "https://gelato-api-live.s3.eu-west-1.amazonaws.com/ecommerce/store_product_image/d3682315-ca68-433a-9cb8-12e9f90f9f1f/preview?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA3QM3COBP5QN4IUGA%2F20260627%2Feu-west-1%2Fs3%2Faws4_request&X-Amz-Date=20260627T134759Z&X-Amz-SignedHeaders=host&X-Amz-Expires=86400&X-Amz-Signature=52d2a0a9bfa536c89187ccec429a1bcafda95aa5e39286f9241ced3bc8cf1294"
     ],
     "createdAt": "2026-06-27T14:19:42.734Z",
-    "updatedAt": "2026-07-08T15:20:10.342Z",
+    "updatedAt": "2026-09-25T14:37:50.246Z",
     "printFileUrl": "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/designs/cool-drowsee-back-t-shirt/design.png",
     "relatedProducts": [],
     "pinterestPins": [
