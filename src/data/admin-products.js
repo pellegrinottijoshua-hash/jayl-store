bbdd36bf6ca10b6965af1cc47ef2580f484e9398
pellegrinottijoshua-hash <pellegrinottijoshua@gmail.com>
2026-09-25 16:37:19 +0200

    admin: update images order for cool-machop-back-t-shirt

--- src/data/admin-products.js
+++ src/data/admin-products.js
@@ -11972,7 +11972,7 @@ export const adminProducts = [
       }
     ],
     "image": "/images/cool-machop-back-t-shirt/hf_20260627_131535_78e2bcf5-6042-4e47-b1f0-208d91bc4057.png",
-    "heroImage": "/images/cool-machop-back-t-shirt/hf_20260627_131557_d047a67f-39b7-4864-b8e2-1e59ab897add.png",
+    "heroImage": "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/images/cool-machop-back-t-shirt/hf_20260627_131125_7f19b816-9a72-4c69-89e5-96e15df37ad3.png",
     "detailImage": "/images/cool-machop-back-t-shirt/hf_20260627_131610_c72b0182-fc89-4f04-a7a8-1de37e0eae77.png",
     "images": [
       "/images/cool-machop-back-t-shirt/hf_20260627_131622_52aef132-ee97-4a63-834f-a5aa803119d6.png",
@@ -12262,7 +12262,7 @@ export const adminProducts = [
       "https://gelato-api-live.s3.eu-west-1.amazonaws.com/ecommerce/store_product_image/127b88c4-5de9-4348-b80f-b23b5b557ea1/preview?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA3QM3COBP5QN4IUGA%2F20260627%2Feu-west-1%2Fs3%2Faws4_request&X-Amz-Date=20260627T132343Z&X-Amz-SignedHeaders=host&X-Amz-Expires=86400&X-Amz-Signature=fe1beefa53765a92361f398d971f3682a2ceba8b18b7c02fec62ecedb51998b1"
     ],
     "createdAt": "2026-06-27T13:31:16.749Z",
-    "updatedAt": "2026-07-15T13:31:18.150Z",
+    "updatedAt": "2026-09-25T14:37:19.451Z",
     "printFileUrl": "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/designs/cool-machop-back-t-shirt/design.png",
     "relatedProducts": [],
     "pinterestPins": [
