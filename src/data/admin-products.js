3c9b929e825fccc3e3ea4ae96b5d06c3cfab98c3
pellegrinottijoshua-hash <pellegrinottijoshua@gmail.com>
2026-09-25 16:31:41 +0200

    admin: update images order for psyduck-pok-mon-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift-gildan-64000

--- src/data/admin-products.js
+++ src/data/admin-products.js
@@ -8009,7 +8009,7 @@ export const adminProducts = [
       }
     ],
     "image": "/images/psyduck-pok-mon-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift-gildan-64000/hf_20260625_162149_cffc15ac-a18b-4f8c-89ec-c5031a9fcb6c.png",
-    "heroImage": "/images/psyduck-pok-mon-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift-gildan-64000/hf_20260625_162554_6a3050de-9ecc-4fb5-8b9b-a32fd5b2785b.png",
+    "heroImage": "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/images/psyduck-pok-mon-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift-gildan-64000/hf_20260625_163702_2f8a607b-0025-455c-bb27-db570f8b7bb0.png",
     "detailImage": "/images/psyduck-pok-mon-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift-gildan-64000/hf_20260625_163042_4bd654b5-24fd-4623-810d-70737455f9df.png",
     "images": [
       "/images/psyduck-pok-mon-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift-gildan-64000/hf_20260625_163132_23f1184e-a260-4913-bf5f-689d53d9a342.png",
@@ -8262,7 +8262,7 @@ export const adminProducts = [
       "https://gelato-api-live.s3.eu-west-1.amazonaws.com/ecommerce/store_product_image/9428ee03-1bde-4249-8123-6dfdbf224b07/preview?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA3QM3COBP5QN4IUGA%2F20260626%2Feu-west-1%2Fs3%2Faws4_request&X-Amz-Date=20260626T110339Z&X-Amz-SignedHeaders=host&X-Amz-Expires=86400&X-Amz-Signature=8a90b0e657ef4622b2b89ff21df20a17bf4dc9d8efb381f341abe0ff6d350e08"
     ],
     "createdAt": "2026-06-26T12:23:42.147Z",
-    "updatedAt": "2026-07-14T13:18:29.545Z",
+    "updatedAt": "2026-09-25T14:31:41.663Z",
     "relatedProducts": [],
     "printFileUrl": "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/designs/psyduck-pok-mon-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift-gildan-64000/design.png",
     "pinterestPins": [
