ca6dde68946a7e1be72567900fe1a92f91c38989
pellegrinottijoshua-hash <pellegrinottijoshua@gmail.com>
2026-09-25 16:35:31 +0200

    admin: update images order for cool-alakazam-back-t-shirt

--- src/data/admin-products.js
+++ src/data/admin-products.js
@@ -10767,7 +10767,7 @@ export const adminProducts = [
       }
     ],
     "image": "/images/alakazam-pok-mon-back-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift/hf_20260626_161153_365c3356-8402-4d57-89cb-613aa7c3d244.png",
-    "heroImage": "/images/alakazam-pok-mon-back-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift/hf_20260626_161220_0af2ad1d-118b-4a78-8a5f-87420da4eed8.png",
+    "heroImage": "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/images/cool-alakazam-back-t-shirt/hf_20260626_161708_91c86649-11d4-4339-9bba-ee98753ef3cc.png",
     "detailImage": "/images/alakazam-pok-mon-back-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift/hf_20260626_161725_e0ba5edd-4da4-4788-a4fc-141e486be559.png",
     "images": [
       "/images/alakazam-pok-mon-back-t-shirt-cool-anime-fan-art-gift-retro-90s-style-funny-pok-mon-lover-gift/hf_20260626_161741_ee168f87-8b30-4711-835e-fccbbe776aed.png",
@@ -11057,7 +11057,7 @@ export const adminProducts = [
       "https://gelato-api-live.s3.eu-west-1.amazonaws.com/ecommerce/store_product_image/1bf5482b-d85d-4f96-964f-211cb4aeb01c/preview?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIA3QM3COBP5QN4IUGA%2F20260626%2Feu-west-1%2Fs3%2Faws4_request&X-Amz-Date=20260626T161532Z&X-Amz-SignedHeaders=host&X-Amz-Expires=86400&X-Amz-Signature=08c18def25e89a934e05186ef5aa825399b9f2313152ed86eafa74afe3ac093f"
     ],
     "createdAt": "2026-06-26T16:31:53.176Z",
-    "updatedAt": "2026-07-15T13:30:10.145Z",
+    "updatedAt": "2026-09-25T14:35:31.302Z",
     "printFileUrl": "https://raw.githubusercontent.com/pellegrinottijoshua-hash/jayl-store/main/public/designs/cool-alakazam-back-t-shirt/design.png",
     "relatedProducts": [],
     "pinterestPins": [
